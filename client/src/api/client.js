dcb882c8ef4b06bff7de44d8ad450d03eae1939c
Aviral Srivastava <kanha23srivastava@gmail.com>
2025-08-14 16:06:14 +0530

    Add API client setup with Axios and request interceptor for authorization

---
+++ client/src/api/client.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { store } from "../app/store";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + "/",
+  withCredentials: false
+});
+
+api.interceptors.request.use((config) => {
+  const state = store.getState();
+  const token = state.auth.token;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
