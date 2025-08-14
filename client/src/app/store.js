4448616837158f93762da7037a64f2ba68514eb3
Aviral Srivastava <kanha23srivastava@gmail.com>
2025-08-14 16:09:03 +0530

    Add comments reducer to the store for state management of video comments

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import videoReducer from "../features/videos/videoSlice";
+import commentReducer from "../features/comments/commentSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    videos: videoReducer
+    videos: videoReducer,
+    comments: commentReducer
   }
 });
